13296315bd7433a15a5ff6ec5514e69ea01fd697
Chris Core <chriscore361@googlemail.com>
2018-04-24 16:29:20 +0100

    Update main.rs
    added example exercise

--- exercises/hello/src/main.rs
+++ exercises/hello/src/main.rs
@@ -1,3 +1,41 @@
+trait Greetable {
+    fn name(&self) -> String;
+}
+
+struct Person {
+    first: String,
+    last: String
+}
+
+impl Greetable for Person {
+    fn name(&self) -> String {
+        format!("{} {}", self.first, self.last)
+    }
+}
+
+impl Person {
+    pub fn new(first: &str, last: &str) -> Self {
+        Person {
+            first: first.to_string(),
+            last: last.to_string(),
+        }
+    }
+}
+
+fn greet<T>(greetee: &T)
+    where T: Greetable + ?Sized
+{
+    println!("Hello, {}", greetee.name());
+}
+
+impl Greetable for str {
+    fn name(&self) -> String {
+        self.to_string()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    greet("World");
+    let me = Person::new("Crisp", "Thinking");
+    greet(&me);
 }
